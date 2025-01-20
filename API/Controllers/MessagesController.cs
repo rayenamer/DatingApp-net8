d66be6b4c8148c9c10cc84364e36d5ee9217e0e4
rayenamer <rayen.ameur@esprit.tn>
2025-01-20 18:09:28 +0100

    end of part 15

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,99 @@
+using System;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+[Authorize]
+
+public class MessagesController
+(
+    IMessageRepository messagesRepository,
+    IUserRepository userRepository,
+    IMapper mapper
+) : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateMessage
+    (CreateMessageDto createMessageDto)
+    {
+        var username = User.GetUserName();
+        if(username ==createMessageDto.RecipientUsername.ToLower())
+            return BadRequest("You cannot message yourself");
+        
+        var sender = await userRepository.GetUserByUsernameAsync(username);
+        var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+
+        if(recipient ==null || sender== null) return BadRequest("cannot send messaage");
+
+        var message =new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+
+        messagesRepository.AddMessage(message);
+
+        if(await messagesRepository.SaveAllAsync())
+            return Ok(mapper.Map<MessageDto>(message));
+
+        return BadRequest("failed to save message");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageDto>>>
+     GetMessagesForUser([FromQuery]MessageParams messageParams)
+     {
+        messageParams.Username = User.GetUserName();
+
+        var messages = await messagesRepository
+        .GetMessagesForUser(messageParams);
+
+        Response.AddPaginationHeader(messages);
+        return messages;
+     }
+
+     [HttpGet("thread/{username}")]
+     public async Task<ActionResult<IEnumerable<MessageDto>>>
+     GetMessageThread(string username)
+     {
+        var currentUsername = User.GetUserName();
+        return Ok(await messagesRepository
+        .GetMessageThread(currentUsername,username));
+     }
+
+     [HttpDelete("{id}")]
+     public async Task<ActionResult> DeleteMessage(int id)
+     {
+        var username = User.GetUserName();
+
+        var message = await messagesRepository.GetMessage(id);
+
+        if(message == null) return BadRequest("Cannot delete this message");
+
+        if(message.SenderUsername != username 
+        && message.RecipientUsername != username)
+            return Forbid();
+
+        if(message.SenderUsername == username) message.SenderDeleted = true;
+        if(message.RecipientUsername == username) message.RecipientDeleted = true;
+
+        if(message is {SenderDeleted: true, RecipientDeleted: true}) {
+            messagesRepository.DeleteMessage(message);
+        }
+
+        if(await messagesRepository.SaveAllAsync()) return Ok();
+
+        return BadRequest("Porblem deleting the message");
+     }
+}
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -21,9 +21,10 @@ public static class ApplicationServiceExtensions
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ILikesRepository,LikesRepository>();
+        services.AddScoped<IMessageRepository,MessageRepository>();
         services.AddScoped<IPhotoService, PhotoService>();
+        
         services.AddScoped<LogUserActivity>();
-
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
